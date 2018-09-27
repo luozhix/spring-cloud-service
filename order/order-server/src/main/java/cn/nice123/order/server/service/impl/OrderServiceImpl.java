0f45631578349ff767ae119106c2b66f44aeaeef
luozhix <495209111@qq.com>
2018-09-27 10:23:46 +0800

    改造order服务成多模块

--- order/order-server/src/main/java/cn/nice123/order/server/service/impl/OrderServiceImpl.java
+++ order/order-server/src/main/java/cn/nice123/order/server/service/impl/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.nice123.order.server.dataobject.OrderDetail;
 import cn.nice123.order.server.dataobject.OrderMaster;
@@ -23,63 +24,61 @@ import cn.nice123.product.common.DecreaseStockInput;
 import cn.nice123.product.common.ProductInfoOutput;
 
 /**
- * Created by 廖师兄
- * 2017-12-10 16:44
+ * Created by 廖师兄 2017-12-10 16:44
  */
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    @Autowired
-    private OrderDetailRepository orderDetailRepository;
+	@Autowired
+	private OrderDetailRepository orderDetailRepository;
 
-    @Autowired
-    private OrderMasterRepository orderMasterRepository;
+	@Autowired
+	private OrderMasterRepository orderMasterRepository;
 
-    @Autowired
-    private ProductClient productClient;
+	@Autowired
+	private ProductClient productClient;
 
-    @Override
-    public OrderDTO create(OrderDTO orderDTO) {
-        String orderId = KeyUtil.genUniqueKey();
+	@Override
+	@Transactional
+	public OrderDTO create(OrderDTO orderDTO) {
+		String orderId = KeyUtil.genUniqueKey();
 
-       //查询商品信息(调用商品服务)
-        List<String> productIdList = orderDTO.getOrderDetailList().stream()
-                .map(OrderDetail::getProductId)
-                .collect(Collectors.toList());
-        List<ProductInfoOutput> productInfoList = productClient.listForOrder(productIdList);
+		// 查询商品信息(调用商品服务)
+		List<String> productIdList = orderDTO.getOrderDetailList().stream().map(OrderDetail::getProductId)
+				.collect(Collectors.toList());
+		List<ProductInfoOutput> productInfoList = productClient.listForOrder(productIdList);
 
-       //计算总价
-        BigDecimal orderAmout = new BigDecimal(BigInteger.ZERO);
-        for (OrderDetail orderDetail: orderDTO.getOrderDetailList()) {
-            for (ProductInfoOutput productInfo: productInfoList) {
-                if (productInfo.getProductId().equals(orderDetail.getProductId())) {
-                    //单价*数量
-                    orderAmout = productInfo.getProductPrice()
-                            .multiply(new BigDecimal(orderDetail.getProductQuantity()))
-                            .add(orderAmout);
-                    BeanUtils.copyProperties(productInfo, orderDetail);
-                    orderDetail.setOrderId(orderId);
-                    orderDetail.setDetailId(KeyUtil.genUniqueKey());
-                    //订单详情入库
-                    orderDetailRepository.save(orderDetail);
-                }
-            }
-        }
+		// 计算总价
+		BigDecimal orderAmout = new BigDecimal(BigInteger.ZERO);
+		for (OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
+			for (ProductInfoOutput productInfo : productInfoList) {
+				if (productInfo.getProductId().equals(orderDetail.getProductId())) {
+					// 单价*数量
+					orderAmout = productInfo.getProductPrice()
+							.multiply(new BigDecimal(orderDetail.getProductQuantity())).add(orderAmout);
+					BeanUtils.copyProperties(productInfo, orderDetail);
+					orderDetail.setOrderId(orderId);
+					orderDetail.setDetailId(KeyUtil.genUniqueKey());
+					// 订单详情入库
+					orderDetailRepository.save(orderDetail);
+				}
+			}
+		}
 
-       //扣库存(调用商品服务)
-        List<DecreaseStockInput> decreaseStockInputList = orderDTO.getOrderDetailList().stream()
-                .map(e -> new DecreaseStockInput(e.getProductId(), e.getProductQuantity()))
-                .collect(Collectors.toList());
-        productClient.decreaseStock(decreaseStockInputList);
+		// 扣库存(调用商品服务)
+		List<DecreaseStockInput> decreaseStockInputList = orderDTO.getOrderDetailList().stream()
+				.map(e -> new DecreaseStockInput(e.getProductId(), e.getProductQuantity()))
+				.collect(Collectors.toList());
+		productClient.decreaseStock(decreaseStockInputList);
 
-        //订单入库
-        OrderMaster orderMaster = new OrderMaster();
-        orderDTO.setOrderId(orderId);
-        BeanUtils.copyProperties(orderDTO, orderMaster);
-        orderMaster.setOrderAmount(orderAmout);
-        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
-        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
-        orderMasterRepository.save(orderMaster);
-        return orderDTO;
-    }
+		// 订单入库
+		OrderMaster orderMaster = new OrderMaster();
+		orderDTO.setOrderId(orderId);
+		BeanUtils.copyProperties(orderDTO, orderMaster);
+		orderMaster.setOrderAmount(orderAmout);
+		orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+		orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+		orderMasterRepository.save(orderMaster);
+		return orderDTO;
+	}
 }
